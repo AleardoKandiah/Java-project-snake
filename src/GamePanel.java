d367094b1e1ff601fffbaf71fe68574721a58250
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-08-08 14:52:51 +0100

    game panel boilerplate

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -7,13 +7,24 @@ import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
 
+ // create matrix for unit size of objects
+
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
-
-    // create matrix for unit size of objects
     static final int UNIT_SIZE= 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
 
+    // create matrix to hold coordinates and units of snake
+    final int x[] = new int[GAME_UNITS]; 
+    final int y[] = new int[GAME_UNITS]; 
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+
+    
     // create constructor game methods
     GamePanel(){
 
