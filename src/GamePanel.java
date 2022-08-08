63871315f8e4d1a093be514d3a65ed26c4b02a35
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-08-08 14:47:36 +0100

    game panel boilerplate

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -10,7 +10,8 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
 
-    // create matrix for unit size
+    // create matrix for unit size of objects
+    static final int UNIT_SIZE= 25;
 
     // create constructor game methods
     GamePanel(){
