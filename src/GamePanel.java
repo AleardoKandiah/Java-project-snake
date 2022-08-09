dfbe187c81d765e69a9815ed49818b9830bdc5d8
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-08-09 11:21:39 +0100

    game panel game settings

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -30,7 +30,9 @@ public class GamePanel extends JPanel implements ActionListener {
     GamePanel(){
         random = new Random();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
-
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
     }
     public void startGane() {
 
