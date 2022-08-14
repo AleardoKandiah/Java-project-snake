02f1e5c032680e3a2d5cf6fd6e8fe32001ed6a87
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-08-14 16:09:32 +0100

    snake bodyh

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -129,7 +129,7 @@ public class GamePanel extends JPanel implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stu
+        // TODO Auto-generated method stub
                 if(running) {
                     move();
                     checkApple();
