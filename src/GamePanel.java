71302cab6704b7d9e8bae4a432a647e8b3af10fc
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-10 18:14:33 +0000

    gameframe action

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,7 +3,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
 
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel implements ActionListener, java.awt.event.ActionListener{
 
      // create matrix for unit size of objects
 
