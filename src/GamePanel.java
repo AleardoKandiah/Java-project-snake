45a137fa83f7be47b632923df112cf5ab35b2817
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-10 18:14:52 +0000

    gameframe action

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener, java.awt.event.ActionListener{
 
-     // create matrix for unit size of object
+     // create matrix for unit size of objec
 
 	static final int SCREEN_WIDTH = 1300;
 	static final int SCREEN_HEIGHT = 750;
